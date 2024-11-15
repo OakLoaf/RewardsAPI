58c65008ff60289c2068ef3a9ade0c454f59ad87
OakLoaf <oak@beaconstudios.org>
2024-11-15 00:26:31 +0000

    Added getRewards method to WrapperReward

--- api/src/main/java/org/lushplugins/rewardsapi/api/reward/WrapperReward.java
+++ api/src/main/java/org/lushplugins/rewardsapi/api/reward/WrapperReward.java
@@ -22,4 +22,14 @@ public abstract class WrapperReward extends Reward {
 
         this.rewards = rewardMaps.stream().map(rewardMap -> Reward.loadReward(rewardMap, rewardMap.toString())).toList();
     }
+
+    /**
+     * It is possible that not all rewards within the wrapper would be received by a player depending on the
+     * subclass implementation so use this carefully
+     *
+     * @return the list of rewards in this wrapper
+     */
+    public List<Reward> getRewards() {
+        return rewards;
+    }
 }
