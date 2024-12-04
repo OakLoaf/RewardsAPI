a0e67ea3bf1e8214e6ccbb205cb11c8f007a001e
OakLoaf <oak@beaconstudios.org>
2024-12-04 16:59:00 +0000

    Bumped version to 0.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ allprojects {
     apply(plugin="com.github.johnrengelman.shadow")
 
     group = "org.lushplugins"
-    version = "0.3.1"
+    version = "0.3.2"
 
     repositories {
         mavenCentral()
